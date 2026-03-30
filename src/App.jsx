f1e568bb9b5bce96c7b6b6a96c39361334976a16
Akrame-Amine Dridi <aadridi@gmail.com>
2026-03-30 23:58:51 +0200

    Starting working on App.jsx structure and 3 compo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react';
+import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
+import Experience from './components/Experience';
 import './App.css';
 
 function App() {
+	const [isEditing, setIsEditing] = useState(true);
+
 	return (
 		<>
-			<h1>Hello World!</h1>
+			{isEditing && (
+				<form>
+					<GeneralInfo />
+					<Education />
+					<Experience />
+				</form>
+			)}
+			{!isEditing && (
+				<div>
+					<GeneralInfo />
+					<Education />
+					<Experience />
+				</div>
+			)}
 		</>
 	);
 }
---
+++ src/components/Experience.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function Experience() {
+	return (
+		<>
+			<h2>Salut les amis!</h2>
+		</>
+	);
+}
